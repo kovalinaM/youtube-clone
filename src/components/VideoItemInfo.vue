841c879c6e05e9663f84d748970d95fadea1ae14
kovalinaM <sunshining1106@mail.ru>
2023-12-12 10:38:19 +0300

    Разбиение на компоненты Video

---
+++ src/components/VideoItemInfo.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex items-start mt-3">
+        <img src="https://i.ytimg.com/vi/TiBCLraUFvA/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLASU2Kfpcm7HQhJdpWDf9uX5GKbGg"
+            class="mr-3 rounded-full w-9 h-9" alt="">
+        <div class="text-sm">
+            <span class="font-semibold text-gray-800">Video title</span>
+            <div class="mt-1 flex items-center">
+                <span>Channel name</span>
+                <BaseIcon name="checkCircle" class="w-3.5 h-3.5 ml-1" ></BaseIcon>
+            </div>
+            <div>
+                <span>1K views</span>
+                &middot;
+                <span>1 day ago</span>
+            </div>
+        </div>
+        <button
+        class="-mt-1 ml-auto p-1 opacity-0 group-hover:opacity-100 text-gray-500 hover:text-gray-700 focus:outline-none">
+        <BaseIcon name="dotsVertical"></BaseIcon>
+    </button>
+</div>
+</template>
+
+<script>
+import BaseIcon from './BaseIcon.vue'
+
+export default {
+    components: {BaseIcon}
+}
+</script>
---
+++ src/components/VideoItemThumbnail.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="relative">
+        <img
+            src="https://i.ytimg.com/vi/ANj7qUgzNq4/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCEC_xrlasDyU31TouJxFudHUrsPQ">
+        <span
+            class="opacity-0 group-hover:opacity-100 bg-opacity-60 absolute top-0 right-0 bg-black text-white rounded-sm m-1 p-1">
+            <BaseIcon  name="clock" class="w-5 h-5"></BaseIcon>
+        </span>
+        <span
+        class="opacity-0 group-hover:opacity-100 bg-opacity-60 absolute top-8 right-0 bg-black text-white rounded-sm m-1 p-1">
+        <BaseIcon  name="menuAlt" class="w-5 h-5"></BaseIcon>
+    </span>
+    <span
+        class="opacity-100 group-hover:opacity-0 duration-500 absolute bottom-0 right-0 bg-black text-white rounded-sm m-1 p-1 text-xs font-semibold">6:35</span>
+</div>
+</template>
+
+<script>
+import BaseIcon from './BaseIcon.vue'
+
+export default {
+    components: {BaseIcon}
+}
+</script>
+
