0f35d7de8ac2162036a669f8a1b4ed229ba92cbe
kovalinaM <sunshining1106@mail.ru>
2023-12-26 10:52:31 +0300

    Добавлена анимация кнопок в блоках для видео

--- src/components/VideoItemThumbnail.vue
+++ src/components/VideoItemThumbnail.vue
@@ -2,14 +2,8 @@
     <div class="relative">
         <img
             src="https://i.ytimg.com/vi/ANj7qUgzNq4/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCEC_xrlasDyU31TouJxFudHUrsPQ">
-        <span
-            class="opacity-0 group-hover:opacity-100 bg-opacity-60 absolute top-0 right-0 bg-black text-white rounded-sm m-1 p-1">
-            <BaseIcon  name="clock" class="w-5 h-5"></BaseIcon>
-        </span>
-        <span
-        class="opacity-0 group-hover:opacity-100 bg-opacity-60 absolute top-8 right-0 bg-black text-white rounded-sm m-1 p-1">
-        <BaseIcon  name="menuAlt" class="w-5 h-5"></BaseIcon>
-    </span>
+        <VideoItemThumbnailButton icon="clock" label="Watch later" class="top-0"/>
+        <VideoItemThumbnailButton icon="menuAlt" label="Add to queue" class="top-8"/>
     <span
         class="opacity-100 group-hover:opacity-0 duration-500 absolute bottom-0 right-0 bg-black text-white rounded-sm m-1 p-1 text-xs font-semibold">6:35</span>
 </div>
@@ -17,9 +11,10 @@
 
 <script>
 import BaseIcon from './BaseIcon.vue'
+import VideoItemThumbnailButton from './VideoItemThumbnailButton.vue'
 
 export default {
-    components: {BaseIcon}
+    components: {BaseIcon, VideoItemThumbnailButton}
 }
 </script>
 
